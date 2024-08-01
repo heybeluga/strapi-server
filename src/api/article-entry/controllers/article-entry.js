5da12f1ecacf4566f23e7df2eec5532760b43271
emiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-08-01 13:10:47 -0300

    add custom endpoints to get draft articles

--- src/api/article-entry/controllers/article-entry.js
+++ src/api/article-entry/controllers/article-entry.js
@@ -6,4 +6,82 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article-entry.article-entry');
+module.exports = createCoreController('api::article-entry.article-entry'
+    , ({strapi})=>({
+    async findDrafts(ctx){
+        // should be an easier way to populate every field
+        const draftArticles = await strapi.entityService.findMany('api::article-entry.article-entry',{
+            filters:{
+                publishedAt: {
+                    $eq:null
+                }
+            },
+            populate: {
+                ArticleSummary: {
+                    populate: ['summaryBullets']
+                },
+                articleText: {
+                    populate: ['paragraphText']
+                },
+                titleImage: {
+                    populate: ['imageHeader', 'image']
+                }
+            }
+        })
+
+        
+        if (draftArticles.length === 0) {
+            ctx.body = {
+              data: null,
+              error: {
+                status: 404,
+                name: "NotFoundError",
+                message: "Not Found",
+                details: {},
+              },
+            };
+          }
+          return ctx.body = {data:draftArticles};
+    }
+    ,
+    async findDraftBySlug(ctx){
+        const {slug} = ctx.params;
+        // should be an easier way to populate every field
+        const draftArticle = await strapi.entityService.findMany('api::article-entry.article-entry',{
+            filters:{
+                publishedAt: {
+                    $eq:null
+                },
+                articleSlug: {
+                    $eq:slug
+                }
+            },
+            populate: {
+                ArticleSummary: {
+                    populate: ['summaryBullets']
+                },
+                articleText: {
+                    populate: ['paragraphText']
+                },
+                titleImage: {
+                    populate: ['imageHeader', 'image']
+                }
+            }
+        })
+
+        
+        if (draftArticle.length === 0) {
+            ctx.body = {
+              data: null,
+              error: {
+                status: 404,
+                name: "NotFoundError",
+                message: "Not Found",
+                details: {},
+              },
+            };
+          }
+          return ctx.body = {data: draftArticle[0]};
+    }
+})
+);

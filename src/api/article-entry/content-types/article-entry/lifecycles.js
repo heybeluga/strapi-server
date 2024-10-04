4d097d1218cf1ce6fc96c8d396ca82560d90c7c5
emiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-10-04 11:11:01 -0300

    delete trigger on article delete

--- src/api/article-entry/content-types/article-entry/lifecycles.js
+++ src/api/article-entry/content-types/article-entry/lifecycles.js
@@ -33,35 +33,4 @@ module.exports = {
       return await strapi.webhookRunner.run(vercelWebhook, "deploy", result);
     }
   },
-
-  async beforeDelete(event) {
-    const { result, params } = event;
-    // check if deleted article has at least one of the tags (only check for published articles)
-    const entry = await strapi.entityService.findMany(
-      "api::article-entry.article-entry",
-      {
-        filters: {
-          id: params.where.id,
-          publishedAt: {
-            $ne: null,
-          },
-          $or: TAGS.map((tag) => ({
-            tags: {
-              tagValue: tag,
-            },
-          })),
-        },
-      }
-    );
-
-    const webhooks = await strapi.webhookStore.findWebhooks();
-    const vercelWebhook = webhooks.find(
-      (webhook) => webhook.name === "vercel webhook"
-    );
-
-    if (vercelWebhook && entry.length > 0) {
-      // trigger deploy webhook
-      return await strapi.webhookRunner.run(vercelWebhook, "deploy", result);
-    }
-  }
 };

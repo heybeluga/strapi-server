6f3e54bc1a9f8901b5b586a305d13aebe9e0db6c
nicolasdecapite1 <66331876+nicolasdecapite1@users.noreply.github.com>
2024-10-17 09:27:58 -0400

    Revert "[NC]: minor fix"
    This reverts commit 8d6bc9c4ebb9dad2d8245866995a9e2ace66d250.

--- config/server.js
+++ config/server.js
@@ -1,15 +1,15 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'https://strapi.heybeluga.com'),
+  // url: env('URL', 'https://strapi.heybeluga.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-  proxy: {
-    enabled: true,
-    ssl: true,
-  },
+  // proxy: {
+  //   enabled: true,
+  //   ssl: true,
+  // },
 });

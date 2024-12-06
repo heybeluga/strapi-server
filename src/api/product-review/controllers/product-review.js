87e5c59703c0d08156095d8251b6b9d255cad586
emiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-12-06 16:09:19 -0300

    add draft product reviews endpoint

--- src/api/product-review/controllers/product-review.js
+++ src/api/product-review/controllers/product-review.js
@@ -12,6 +12,9 @@ module.exports = createCoreController('api::product-review.product-review',
             const {slug} = ctx.params;
             const productReview  = await strapi.entityService.findMany('api::product-review.product-review',{
                 filters:{
+                    publishedAt: {
+                        $ne:null
+                    },
                     reviewSlug: {
                         $eq: slug
                     }
@@ -20,11 +23,30 @@ module.exports = createCoreController('api::product-review.product-review',
                 populate: 'deep'
             })
     
-            
             if (productReview.length === 0) {
                 return ctx.throw(404)
             }
-              
             return this.transformResponse(productReview[0])
-    }})
+        },
+        async findDraftBySlug(ctx){
+            const {slug} = ctx.params;
+            const productReview  = await strapi.entityService.findMany('api::product-review.product-review',{
+                filters:{
+                    publishedAt: {
+                        $eq:null
+                    },
+                    reviewSlug: {
+                        $eq: slug
+                    }
+                },
+                // @ts-ignore
+                populate: 'deep'
+            })
+        
+            if (productReview.length === 0) {
+                return ctx.throw(404)
+            }
+            return this.transformResponse(productReview[0])
+        }
+    })
 );
--- src/api/product-review/routes/product-review-custom.js
+++ src/api/product-review/routes/product-review-custom.js
@@ -5,6 +5,11 @@ module.exports = {
             method: 'GET',
             "path": "/product-reviews/slug/:slug",
             "handler": "product-review.findBySlug",
+        },
+        {
+            method: 'GET',
+            "path": "/product-reviews/draft/:slug",
+            "handler": "product-review.findDraftBySlug",
         }
     ]
 }

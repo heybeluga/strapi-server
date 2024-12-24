6b212bc085220ae5f979f6e49fb12c253b469670
emiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-12-24 01:35:43 -0300

    add findAllContent endpoint to merge articles and product reviews

--- src/api/article-entry/controllers/article-entry.js
+++ src/api/article-entry/controllers/article-entry.js
@@ -1,70 +1,67 @@
 'use strict';
 
+// Util function to replace key in object with another key
+function replaceKey(obj, oldKey, newKey) {
+    if (Array.isArray(obj)) {
+      // Iterate over arrays recursively
+      return obj.map(item => replaceKey(item, oldKey, newKey));
+    } else if (typeof obj === 'object' && obj !== null) {
+      // Create a new object with replaced keys where applicable
+      return Object.keys(obj).reduce((acc, key) => {
+        const value = obj[key];
+        if (key === oldKey) {
+          acc[newKey] = replaceKey(value, oldKey, newKey);
+        } else {
+          acc[key] = replaceKey(value, oldKey, newKey);
+        }
+        return acc;
+      }, {});
+    }
+    // Return non-object values as-is
+    return obj;
+}
+
 /**
  * article-entry controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController, transformResponse } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::article-entry.article-entry'
     , ({strapi})=>({
     async findDrafts(ctx){
         // should be an easier way to populate every field
         const draftArticles = await strapi.entityService.findMany('api::article-entry.article-entry',{
-            filters:{
-                publishedAt: {
-                    $eq:null
-                }
-            },
+            publicationState: 'preview',
             // @ts-ignore
             populate: 'deep'
         })
 
-        
         if (draftArticles.length === 0) {
-            return ctx.body = {
-              data: null,
-              error: {
-                status: 404,
-                name: "NotFoundError",
-                message: "Not Found",
-                details: {},
-              },
-            };
-          }
-          return ctx.body = {data:draftArticles};
+            return ctx.throw(404, 'No draft articles found');
+        }
+        return this.transformResponse(draftArticles);
     }
     ,
     async findDraftBySlug(ctx){
         const {slug} = ctx.params;
-        // should be an easier way to populate every field
         const draftArticle = await strapi.entityService.findMany('api::article-entry.article-entry',{
             filters:{
-                publishedAt: {
-                    $eq:null
-                },
                 articleSlug: {
                     $eq:slug
                 }
             },
+            publicationState: 'preview',
             // @ts-ignore
             populate: 'deep'
         })
 
         
         if (draftArticle.length === 0) {
-            return ctx.body = {
-              data: null,
-              error: {
-                status: 404,
-                name: "NotFoundError",
-                message: "Not Found",
-                details: {},
-              },
-            };
-          }
-          const {id, ...articleData} = draftArticle[0];
-          return ctx.body = {data: {id, attributes: articleData}};
+            return ctx.throw(404, 'Article not found');
+        }
+            
+        return this.transformResponse(draftArticle[0]);
     },
     async findByTags(ctx){
         try {
@@ -139,6 +136,73 @@ module.exports = createCoreController('api::article-entry.article-entry'
         } catch (error) {
             return ctx.throw(500)
         }
+    },
+    async findAllContent(ctx){
+        // query articles entries
+        const { results, pagination } = await strapi
+            .service("api::article-entry.article-entry")
+            .find({
+                populate: {
+                    titleImage: {
+                        populate: ['imageHeader', 'image']
+                    },
+                    tags: {
+                        populate: ['tagValue']                    
+                    },
+                },
+                sort: {
+                    migratedPublishedDate: 'desc'
+                },
+                pagination: ctx.query.pagination,
+                filters: replaceKey(ctx.query.filters, 'title', 'ArticleTitle')
+            });
+
+        // query product reviews
+        const { results: reviews, pagination: reviewsPagination } = await strapi
+            .service("api::product-review.product-review")
+            .find({
+                populate: {
+                    reviewImage: {
+                        populate: ['imageHeader', 'image']
+                    },
+                    tags: {
+                        populate: ['tagValue']                    
+                    },
+                },
+                sort: {
+                    migratedPublishedDate: 'desc'
+                },
+                pagination: ctx.query.pagination,
+                filters: replaceKey(ctx.query.filters, 'title', 'reviewTitle')
+            });
+        
+        //@ts-ignore 
+        const {data:transformedReviews, meta: reviewsMeta} = strapi.controller('api::product-review.product-review').transformResponse(reviews, { pagination: reviewsPagination})
+        const reviewHasNext = reviewsMeta.pagination.total > reviewsMeta.pagination.pageSize * reviewsMeta.pagination.page;
+        //@ts-ignore 
+        const {data: transformedArticles, meta: articlesMeta} = this.transformResponse(results, { pagination });
+        const articleHasNext = articlesMeta.pagination.total > articlesMeta.pagination.pageSize * articlesMeta.pagination.page;
+        
+        const pageCount = articlesMeta.pagination.pageCount > reviewsMeta.pagination.pageCount ? articlesMeta.pagination.pageCount : reviewsMeta.pagination.pageCount;
+
+        const mergedMeta = {
+            pagination: {
+                ...articlesMeta.pagination,
+                pageCount: pageCount,
+                hasNext: articleHasNext || reviewHasNext,
+                total: articlesMeta.pagination.total + reviewsMeta.pagination.total
+            }
+        }
+
+        const sortedContent = [...transformedArticles, ...transformedReviews].sort((a, b) => {
+            return new Date(b.attributes.migratedPublishedDate).getTime() - new Date(a.attributes.migratedPublishedDate).getTime();
+        });
+
+        return ctx.body = { 
+            data: sortedContent, 
+            meta: mergedMeta
+        };
+
     }
 })
 );
